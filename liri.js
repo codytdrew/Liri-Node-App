e7f509dfcadc6bc72108b3646ff59fd8aa6589a5
John Florence <codytdrew@gmail.com>
2019-05-04 16:23:16 -0700

    got the movies to populate, but working to figure out why it is calling the bands API

--- liri.js
+++ liri.js
@@ -48,7 +48,7 @@ function concert() {
       if (error.response) {
         // The request was made and the server responded with a status code
         // that falls out of the range of 2xx
-        console.log(error.response.data);
+        console.log(error.response.data[0]);
         console.log(error.response.status);
         console.log(error.response.headers);
       } else if (error.request) {
@@ -75,3 +75,30 @@ function songId() {
   });
 }
 
+// When we call "movieFinder" function, we will be provided a list of information pertaining to the movie
+function movieFinder () {
+  axios
+  .get("http://www.omdbapi.com/?t=" + searchTerm + "&apikey=e8747782")
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+}
+// function executeAction {
+// }
+  )}
